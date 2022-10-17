13e74daf2c571d8b3440846c3e5f519e78901e36
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-18 01:04:47 +0200

    .

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', loadEvent);
+
+const headerComponent = function (title) {
+  return `
+        <header>
+            <h1>${title}</h1>
+            <span class="material-icons" style="font-size: 36px; color: #145434">
+                menu
+            </span>
+        </header>
+        <hr/>
+    `;
+};
+
+function loadEvent() {
+  const rootElement = document.getElementById('root');
+
+  const beerCards = beers.cards;
+
+  rootElement.insertAdjacentHTML('beforebegin', headerComponent('Best Beers'));
+
+  for (beer of beerCards) {
+    rootElement.insertAdjacentHTML(
+      'beforeend',
+      beerCardComponent(beer.title, beer.sub, beer.text)
+    );
+  }
+}
+
+const beerCardComponent = function (beerName, beerCompany, beerType) {
+  return `
+          <section class="card">
+              <span class="number">1</span>
+              <div class="inner-card">
+                  <p class="title">${beerName}y</p>
+                  <hr />
+                  <p class="sub">${beerCompany}</p>
+                  <p class="text">${beerType}</p>
+              </div>
+              <button>
+                  <p class="button-text">details</p>
+                  <span class="material-icons" style="font-size: 36px; color: #145434">
+                  arrow_forward
+                  </span>
+              </button>
+          </section>
+      `;
+};
