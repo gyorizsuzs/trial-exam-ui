363e86e8ba752c84dd78e42aab08a0c54026ebd8
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-18 02:23:07 +0200

    .

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ window.addEventListener('load', loadEvent);
 const headerComponent = function (title) {
   return `
         <header>
+        <div class="header-container">
             <h1>${title}</h1>
             <span class="material-icons" style="font-size: 36px; color: #145434">
                 menu
             </span>
+        </div>
         </header>
-        <hr/>
     `;
 };
 
@@ -31,15 +32,16 @@ const beerCardComponent = function (beerId, beerName, beerCompany, beerType) {
   return `
           <section class="card">
               <span class="number">${beerId}</span>
-              <div class="inner-card">
-                  <p class="title">${beerName}y</p>
-                  <hr />
-                  <p class="sub">${beerCompany}</p>
-                  <p class="text">${beerType}</p>
+              <div class="text-container">
+                <p class="title">${beerName}</p>
+                <div class="inner-container">
+                    <p class="sub">${beerCompany}</p>
+                    <p class="text">${beerType}</p>
+                </div>
               </div>
               <button>
                   <p class="button-text">details</p>
-                  <span class="material-icons" style="font-size: 36px; color: #145434">
+                  <span class="material-icons">
                   arrow_forward
                   </span>
               </button>
